03e5dbc362cb80759b11260b2669eefe3033a5f3
Francisco <melicias1999@gmail.com>
2022-05-04 19:29:58 +0100

    download excel 100 funcional

--- app/Services/TurnoService.php
+++ app/Services/TurnoService.php
@@ -58,7 +58,15 @@ class TurnoService
             header('Content-Type: application/vnd.ms-excel');
             header('Content-Disposition: attachment;filename="' . $turno->tipo . ($turno->numero) . '_' . $turno->cadeira->nome . '.xls"');
             header('Cache-Control: max-age=0');
+            header('name: max-age=0');
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application');
+            header('Access-Control-Expose-Headers: Content-Disposition');
+    
             ob_end_clean();
+            //$Excel_writer->save('php://output');
             $Excel_writer->save('php://output');
             return ['msg' => "hmmmm",'code' => 200];
             //exit();
